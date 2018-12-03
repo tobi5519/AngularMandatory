797bf95b05ae229cc961d69ec4dcba1f860d546a
Andreas <rednex702@hotmail.com>
2018-12-03 15:20:33 +0100

    lavet entities igen

---
+++ src/app/Entities/Baby.ts
@@ -0,0 +1,13 @@
+export class Baby{
+
+  _APIId: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  gender: string; // Female, Male, Other...
+  birthDate: Date; // We learn about js dates...
+  specialNeeds: string;
+  zipCode: number;
+  city: string;
+}
---
+++ src/app/Entities/Sitter.ts
@@ -0,0 +1,16 @@
+export class Sitter{
+
+  _APIId: string;
+  firstname: string;
+  lastname: string;
+  password: string;
+  email: string;
+  gender: string; // Female, Male, Other...
+  birthDate: Date; // We learn about js dates...
+  noCriminalRecord: boolean; // true = no crimes
+  noChildRecord: boolean; // true = not a child criminal
+  hourlyWage: number;
+  zipCode: number;
+  city: string;
+
+}
